1349ed0a1abb39b5f48083ab2134f1e9f7ecefbb
dustinatflatiron <dustin.anderson@flatironschool.com>
2021-05-21 15:54:48 +0000

    added movie class and initialize

---
+++ lib/movie.rb
@@ -0,0 +1,22 @@
+class Movie 
+
+    attr_reader :rank, :title, :rating, :link
+
+    @@all = []
+
+    def initialize(rank, title, rating, link)
+        @rank = rank
+        @title = title
+        @rating = rating 
+        @link = link
+        self.save
+    end
+
+    def save 
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+end
